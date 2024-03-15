1eb6321ee6f06b18a7b759eee58d17e885a42094
Sidnei <sidneisonic@hotmail.com>
2024-03-15 12:55:46 -0300

    aula 64 criando e configurando o config

---
+++ src/contexts/config.ts
@@ -0,0 +1,13 @@
+// Exportar todas os status iniciais
+export const InitialState = {
+
+}
+
+//Exportando todos os reducers
+export const Reducers = (state: InitialStateType, action: ActionType) => {
+
+}
+
+//Exportando todos os types
+export type InitialStateType = typeof InitialState
+export type ActionType = any
