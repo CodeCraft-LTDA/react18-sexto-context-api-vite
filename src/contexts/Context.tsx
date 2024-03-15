669b66b7a3a7fae2199f2e4d4dc5e8e483d48a66
Sidnei <sidneisonic@hotmail.com>
2024-03-15 13:03:20 -0300

    aula 65 ContextAPI criando o Context e o Provider

---
+++ src/contexts/Context.tsx
@@ -0,0 +1,26 @@
+import { createContext, useReducer } from "react";
+import { ActionType, InitialState, InitialStateType, Reducers } from "./config";
+
+type Props = {
+    children: React.ReactNode
+}
+
+type ContextType = {
+    state: InitialStateType
+    dispatch: React.Dispatch<ActionType>
+}
+
+export const Context = createContext<ContextType>({
+    state: InitialState,
+    dispatch: () => null
+})
+
+export const ContextProvider = ({ children }: Props) => {
+    const [state, dispatch] = useReducer(Reducers, InitialState)
+
+    return (
+        <Context.Provider value={{state, dispatch}}>
+            {children}
+        </Context.Provider>
+    )
+}
--- src/contexts/config.ts
+++ src/contexts/config.ts
@@ -4,9 +4,9 @@ export const InitialState = {
 }
 
 //Exportando todos os reducers
-export const Reducers = (state: InitialStateType, action: ActionType) => {
+export const Reducers = (state: InitialStateType, action: ActionType) => ({
 
-}
+})
 
 //Exportando todos os types
 export type InitialStateType = typeof InitialState
